01ff7a57350b56aeb1712d2347b84bc98dc5ac98
Micaela Perez <micaelaperez663@gmail.com>
2022-05-20 17:32:53 -0300

    calendar without styling

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,7 +3,7 @@ import '../styles/Body.css';
 import {useState} from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
-import { Today } from '@mui/icons-material';
+import {DropdownListComponent} from '@syncfusion/ej2-react-dropdowns';
 
 function Body() {
     const [date, setDate] = useState(new Date());
@@ -12,13 +12,18 @@ function Body() {
         setDate(date);
     };
 
+    const [calendar, setCalendar] = useState(false);
+
+    const showCalendar = () => setCalendar(!calendar);
+
     return (
         <div className='body_container'>
             <p className='body_title'>Buscar ofertas en hoteles, casas y mucho más</p>
             <div className='body_input_container'>
-                <input type='text' placeholder='Seleccione el destino' className='body_input' />
-                <Calendar onChange={onChange} value={value} className='body_input' showDoubleView={true} />
-                <button className='body_button'>Buscar</button>
+                <input type='text' placeholder='Seleccione el destino'  className='body_input' />
+                <Calendar onChange={onChange}   value={date} className='calendar_mobile' showDoubleView={false} selectRange={true} />
+                <Calendar onChange={onChange} value={date} className='calendar_desktop' showDoubleView={true} />
+                <button className='body_button' onClick={showCalendar}>Buscar</button>
             </div>
         </div>
     )
