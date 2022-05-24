9a88b8c0f5142511a480aee84f91a07d1b3b9f3a
Micaela Perez <micaelaperez663@gmail.com>
2022-05-24 17:03:31 -0300

    dropdown working

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,20 +5,23 @@ import 'rsuite/dist/rsuite.min.css';
 import DateRangePicker from 'rsuite/DateRangePicker';
 import Categories from './Categories';
 import Cards from './Cards';
+import data from '../utils/city.json';
 
 function Body() {
     const [setDate] = useState(new Date());
-
-    const onChange = (date) => {
-        setDate(date);
-    };
-
     return (
         <>
             <div className='body_container'>
                 <p className='body_title'>Buscar ofertas en hoteles, casas y mucho más</p>
                 <div className='body_input_container'>
-                    <input type='text' placeholder='Seleccione el destino' className='body_input' />
+                    <form className='form'>
+                        <select id="city" name="city" className='form_select'>
+                            <option className="form_option" value="" disabled selected hidden>Selecciona el destino</option>
+                            {data.city.map((item, index) => (
+                            <option key={item.id}>{item.name}</option>
+                        ))}
+                        </select>
+                    </form>
                     <DateRangePicker placeholder="Select Date Range" className='calendar_desktop' />
                     <DateRangePicker placeholder="Select Date Range" oneTap showOneCalendar hoverRange="week" ranges={[]} className='calendar_mobile' />
                     <button className='body_button'>Buscar</button>
