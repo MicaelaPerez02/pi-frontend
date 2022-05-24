86b4fa6d2451e9088284598b4b883fe5c26f442e
Micaela Perez <micaelaperez663@gmail.com>
2022-05-24 12:46:45 -0300

    cards mobile working

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import data from '../utils/products.json';
 import '../styles/Cards.css';
+import { FaMapMarkerAlt } from "react-icons/fa";
 
 function Cards() {
     return (
@@ -10,11 +11,34 @@ function Cards() {
                 {data.products.map((item, index) => {
                     return (
                         <div key={item.id} className='card_box_recomendations'>
-                            <div className='img_container'>
-                                <img className='cards_img' src={item.url} alt={item.name} />
+                            <div className='card_info_container'>
+                                <div className='img_container'>
+                                    <img src={item.url} alt={item.name} />
+                                </div>
+                                <div className='rating'>
+                                    <span><p className='hotel'> HOTEL </p>{
+                                        Array(item.rating)
+                                            .fill()
+                                            .map((_) => (
+                                                <p key={item.rating}>⭐</p>
+                                            ))
+                                    }</span>
+                                    <p className="review">{item.review}</p>
+                                </div>
+                                <div className='title'>
+                                    <p>{item.title}</p>
+                                </div>
+                                <div className='distances'>
+                                    <p><FaMapMarkerAlt className='icon_map' /> {item.ubication}</p>
+                                    <span>{item.watch}</span>
+                                </div>
+                                <div className='description'>
+                                    <p>{item.description}<span className='more'>...continuar leyendo</span></p>
+                                </div>
+                                <div className='button'>
+                                    <button>Ver más</button>
+                                </div>
                             </div>
-                            <p className='cards_name'>{item.title}</p>
-                            <p className='cards_description'>{item.description}</p>
                         </div>
                     )
                 })}
