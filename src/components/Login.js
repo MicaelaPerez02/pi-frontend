41c4a12ac7d7cf6b9330e5a13d09c85eab537e97
Micaela Perez <micaelaperez663@gmail.com>
2022-05-26 09:24:08 -0300

    styling login mobile

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,55 +4,56 @@ import "../styles/Login-Register.css";
 import HeaderLogin from "./Header-Login";
 import "../styles/Header.css";
 import Footer from "./Footer";
-import {useState} from "react";
-import {AiOutlineEyeInvisible, AiOutlineEye} from "react-icons/ai";
+import { useState } from "react";
+import { AiOutlineEyeInvisible, AiOutlineEye } from "react-icons/ai";
 
 function Login() {
-    const [state , setState] = useState(false);
-    const toggleBtn = (e) => {{
-        setState(prevState => !prevState);
+    const [state, setState] = useState(false);
+    const toggleBtn = (e) => {
+        {
+            setState(prevState => !prevState);
+        }
+        e.preventDefault();
     }
-    e.preventDefault();
-
-    
-}
     return (
         <>
-       <HeaderLogin/>
-        <div className="login">
-        
-            <form className="form_container">
+            <HeaderLogin />
+            <div className="login">
 
-                <h1 className="title">Iniciar sesión</h1><br/><br/>
-                <form className="input_container-Login">
-                    <div className="input1">
-                                      <h5 >Correo electronico</h5>
-                    <input  type="email" name="email" placeholder="Email" />
-                    </div> <br/><br/>
-                    <h5 >Contraseña</h5>
-                    <div className="inputWrapper1">
-                    <input className="input1" type={state ? "text": "password"} name="password" placeholder="Password" />
-                    <button className="btn-icon" onClick={toggleBtn}>
-                    {state ?   <AiOutlineEye className="icon-eyeBlind"/>: <AiOutlineEyeInvisible className="icon-eyeBlind"/>}
-                    </button>
-                    </div><hr/>
-                   
+                <form className="form_container">
+
+                    <h1 className="title">Iniciar sesión</h1><br /><br />
+                    <form className="input_container-Login">
+                        <div className="input1">
+                            <h5>Correo electronico</h5>
+                            <input type="email" name="email" placeholder="Email" />
+                        </div> <br /><br />
+                        <div className="input2">
+                            <h5>Contraseña</h5>
+                            <input className="input1" type={state ? "text" : "password"} name="password" placeholder="Password" />
+                            <button className="btn-icon" onClick={toggleBtn}>
+                                {state ? <AiOutlineEye className="icon-eyeBlind" /> : <AiOutlineEyeInvisible className="icon-eyeBlind" />}
+                            </button>
+                        </div><hr />
+                    </form>
+
+                    <div className="btn-container">
+                        <button className="btn_singIn">Ingresar</button>
+                        <br />
+                    </div>
+
+                    <p className="text_register">
+                        ¿Aún no tenes cuenta?{" "}
+                        <Link to="/register" className="btn_register_link">
+                            Registrate
+                        </Link>
+                    </p>
+
 
                 </form>
-                <button className="btn_singIn">Ingresar</button>
-                <br/>
-                <p className="text_register">
-                    ¿Aún no tenes cuenta?{" "}
-                    <Link to="/register" className="btn_register_link">
-                        Registrate
-                    </Link>
-                </p>
-               
-
-            </form>
-        </div>
-        <br/><br/><br/> <br/><br/><br/>
-        <Footer/>
+            </div>
+            <br /><br /><br /> <br /><br /><br />
+            <Footer />
         </>
     );
 }
