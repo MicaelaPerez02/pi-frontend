f277c010cffacd4b9e778af55afdcc713050fed5
Micaela Perez <micaelaperez663@gmail.com>
2022-06-16 14:42:59 -0300

    starting event calendar

--- src/components/ProductInfo/CalendarDetail.js
+++ src/components/ProductInfo/CalendarDetail.js
@@ -4,22 +4,8 @@ import Calendar from 'react-calendar';
 import "../../styles/CalendarDetail.css";
 
 function CalendarDetail() {
-    const minimumDate = {
-        year: 2022,
-        month: 6,
-        day: 8
-    };
-
-    const maximumDate = {
-        year: 2022,
-        month: 7,
-        day: 22
-    }
-
     return (
         <Calendar
-            minimumDate={minimumDate}
-            maximumDate={maximumDate}
             selectRange={true}
         />
     );
--- src/components/ProductInfo/CalendarDetailResponsive.js
+++ src/components/ProductInfo/CalendarDetailResponsive.js
@@ -1,27 +1,17 @@
 import React from 'react'
 import 'react-calendar/dist/Calendar.css';
 import Calendar from 'react-calendar';
+import { useState } from 'react';
 import "../../styles/CalendarDetail.css";
 
 function CalendarDetail() {
-    const minimumDate = {
-        year: 2022,
-        month: 7,
-        day: 8
-    };
-
-    const maximumDate = {
-        year: 2022,
-        month: 8,
-        day: 22
-    }
-
     return (
         <Calendar
-            minimumDate={minimumDate}
-            maximumDate={maximumDate}
             showDoubleView={true}
             selectRange={true}
+            minDate={new Date()}
+            maxDate={new Date(2023, 11, 16)}
+            returnValue="range"
         />
     );
 };
