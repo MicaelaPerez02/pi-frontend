942075c6b5fa4f2f35d8383f6f74eae5bd52a0fd
Micaela Perez <micaelaperez663@gmail.com>
2022-06-17 14:35:21 -0300

    styling carrousel

--- src/components/ProductInfo/CarrouselProduct.js
+++ src/components/ProductInfo/CarrouselProduct.js
@@ -22,13 +22,8 @@ function CarrouselProduct(props) {
                                     style={{ height: '50vh', backgroundImage: `url(${image.url})`, backgroundSize: 'cover'}}>
                                     <div
                                         style={{
-                                            padding: '5px 0',
-                                            width: 140,
-                                            borderBottomRightRadius: '10px',
-                                            boxShadow: '4px 5px 0 rgba(0,0,0,0.2)',
-                                            backgroundColor: 'rgba(255,255,255,0.8)',
+                                            width: 0,
                                         }}>
-                                        {image.title}
                                     </div>
                                 </div>
                             </div>
