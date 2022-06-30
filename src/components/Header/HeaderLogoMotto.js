2ef43a5886cd177a0942de5e7efe3444d6ea3e77
Micaela Perez <micaelaperez663@gmail.com>
2022-06-29 22:25:04 -0300

    fixing city filter to use in only one click

--- src/components/Header/HeaderLogoMotto.js
+++ src/components/Header/HeaderLogoMotto.js
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom';
 
 function HeaderLogoMotto() {
     return (
-        <div className="header_logo_motto">
-            <Link to='/' style={{ textDecoration: 'none' }}>
+        <div className="headerLogoMotto">
+            <Link to='/'>
                 <img
                     className='header_logo'
                     src="https://bucket-pig6.s3.us-east-2.amazonaws.com/Logos+Homu/Recurso+2finals.png"
-                    alt='Logo Empresa'
+                    alt='Logo Homu'
                 />
             </Link>
-            <p className='header_motto'>Estás en el lugar correcto</p>
+            <p className='headerMotto'>Estás en el lugar correcto</p>
         </div>
     )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import DateRangePicker from 'rsuite/DateRangePicker';
 import "../../styles/Components/Navbar.css";
 import '../../styles/Accesories/DatePickerLibrary.css';
@@ -8,6 +8,14 @@ import 'rsuite/dist/rsuite.min.css';
 import CardCities from '../Cards/CardCities';
 
 function Navbar(props) {
+    const navigate = useNavigate();
+
+
+    const handleClick = () => {
+        navigate("/cities/" + localStorage.getItem("cities"));
+        window.location.reload();
+    }
+
     return (
         <div className='navbarContainer'>
             <h2 className="title">Buscar ofertas en hoteles, casas y mucho más</h2>
@@ -17,7 +25,7 @@ function Navbar(props) {
                 <DateRangePicker placeholder="Seleccione el rango de fechas" oneTap showOneCalendar hoverRange="week" format='dd-MM-yyyy' className='datePickerMobile' />
                 <section className="navbarButtonContainer">
                     <Link to={"/cities/" + localStorage.getItem("cities")} >
-                        <button className='navbarButton' onClick={window.location.reload}>Buscar</button>
+                        <button className='navbarButton' onClick={handleClick}>Buscar</button>
                     </Link>
                 </section>
             </section>
